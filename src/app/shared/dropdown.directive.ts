60f27047290e53979d4b1db695b9e1a7ec623881
damenkay <damenkangnaan@yahoo.com>
2023-07-24 23:57:11 +0100

    new

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -4,7 +4,7 @@ import { Directive, HostBinding, HostListener } from "@angular/core";
     selector: '[addDropdown]'
 })
 export class DropdownDirective{
-   @HostBinding('class.keep-Open') isOpen = false
+   @HostBinding('class.open') isOpen = false
    
     @HostListener('click')toggleOpen(){
        this.isOpen = !this.isOpen
